0a87c99264eb9a4a19b59d8aa512719958ab4f51
hallasaleh <halla_imad@hotmail.com>
2019-10-16 10:09:17 -0500

    commet2

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -6,7 +6,7 @@ namespace GitTest
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Master!");
         }
     }
 }
